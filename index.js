7591b9faa00b5dcdc707c428575e2c421495be5d
JTS-Games <1066300@hr.nl>
2025-01-20 10:15:49 +0100

    Fixed server

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ import express, {application} from 'express';
 import mongoose from 'mongoose';
 
 const app = express();
-mongoose.connect(`http://localhost:8080`);
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`);
 
 //so JSON can be used
 app.use(express.json());
 
-// so that this fancy standard can be used
+// so that this fancy standard with a long name can be used
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.json(
         {
-            message: 'Hello there :D'
+            message: 'Looks like this works :D'
         }
     )
+});
+
+app.listen(process.env.EXPRESS_PORT, () => {
+    console.log('Server is running');
 });
